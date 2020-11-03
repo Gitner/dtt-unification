f68676d5641a2f883b33c807155efb905968ea15
Gitner <spam@aruba.it>
2020-11-03 18:18:42 +0100

    Update after Discovery Api change
    Improve code readability

--- discovery.js
+++ discovery.js
@@ -1,10 +1,12 @@
 //crea il dizionario obj e lo scrive come json {canale:link}
-async function objDiscovery(data) {
+async function objDiscovery(json) {
   //nome dello script con aggiunta suffisso 'on' name.js => name.json
   const iJson = path.basename(__filename) + 'on';
-  var obj = {};
-  for (var entry  in data.Data) {
-    obj[data.Data[entry].Title] = data.Data[entry].PlaybackInfoUrl;
+  //template del link al canale
+  const tplLink = 'https://eu2-prod.disco-api.com/playback/channelPlaybackInfo/*?usePreAuth=true'
+  var obj = {}, data = json.included;
+  for (var entry  in data) {
+    obj[data[entry].meta.path.toUpperCase().replace(/-/g, ' ')] = tplLink.replace('*', data[entry].relationships.channel.data.id);
   }
   //scrive il file json contenente la lista dei canali
   fs.writeFile(iJson , JSON.stringify(obj), (err) => {
@@ -19,13 +21,12 @@ const fs = require('fs');
 //libreria per gestire i percorsi dei file
 const path = require('path');
 //contenitore della chiave che permette di accedere alle informazioni
-const urlToken = 'https://dplayproxy.azurewebsites.net/api/config/init';
-//url all'api con le informazioni per riprodurre i flussi  a/v dei canali
-const urlApi = 'https://dplayproxy.azurewebsites.net/api/TvVideo/LiveList';
-//chiama il json con l'AccessToken e lo estrae
+const urlToken = 'https://eu2-prod.disco-api.com/token?realm=dplayit';
+//url all'api con le informazioni per riprodurre i flussi a/v dei canali
+const urlApi = 'https://eu2-prod.disco-api.com/cms/collections/dplay-channels?include=items';
+//chiama il json con il Token e lo estrae
 request(urlToken).then(JSON.parse).then((json) => {
-  const aToken = json.Data.AccessToken;
-  const bearer = aToken.split('__!__')[0];
+  const bearer = json.data.attributes.token;
   //chiama il json con la lista canali ne fa il parsing e crea il file discovery.json
-  request({url: urlApi, headers: {'AccessToken': aToken}}).then(JSON.parse).then(objDiscovery);
+  request({url: urlApi, headers: {'authorization': 'Bearer ' + bearer}}).then(JSON.parse).then(objDiscovery);
 });
