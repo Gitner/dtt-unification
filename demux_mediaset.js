3e32c117ef3869cfc2fc090e7b3ed0ecc2dfdb89
Gitner <spam@aruba.it>
2020-11-24 11:56:12 +0100

    Update after mediaset api change
    New api NowNext implementation

--- demux_mediaset.js
+++ demux_mediaset.js
@@ -1,8 +1,8 @@
 const request = require('request-promise');
-const urlApi = 'https://api-ott-prod-fe.mediaset.net/PROD/play/alive/nownext/v1.0?channelId=';
+const urlApi = 'https://static3.mediasetplay.mediaset.it/apigw/nownext/';
 
-mediaset = async (url) => {
-  const json = await request({uri: urlApi + url, json: true});
-  return (json['response']['tuningInstruction']['urn:theplatform:tv:location:any'][0]['format'] == 'application/x-mpegURL' ? json['response']['tuningInstruction']['urn:theplatform:tv:location:any'][0]['publicUrls'][0] : json['response']['tuningInstruction']['urn:theplatform:tv:location:any'][1]['publicUrls'][0]);
+mediaset = async (callSign) => {
+  const json = await request({uri: urlApi + callSign + '.json', json: true});
+  return (json.response.tuningInstruction['urn:theplatform:tv:location:any'][0]['format'] == 'application/x-mpegURL' ? json.response.tuningInstruction['urn:theplatform:tv:location:any'][0]['publicUrls'][0] : json.response.tuningInstruction['urn:theplatform:tv:location:any'][1]['publicUrls'][0]);
 }
 exports.mediaset = mediaset;
